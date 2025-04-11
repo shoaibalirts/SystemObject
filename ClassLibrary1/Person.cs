f33cc6ee8426d67107eeb55dcd0d5dc76571af98
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-11 11:28:44 +0200

    Add project files.

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string PersonName { set; get; }
+        public string EmailAdd { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            Person p = (Person)obj;
+            if (this.PersonName == p.PersonName && this.EmailAdd == p.EmailAdd)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Person name: " + this.PersonName;
+        }
+    }
+}
---
+++ SystemObject/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary1;
+using static System.Console;
+
+namespace SystemObject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Object obj = new Person()
+            {
+                PersonName = "Shoaib",
+                EmailAdd = "shoaibali.rts@gmail.com"
+            };
+            // access methods
+            WriteLine(obj.Equals(new Person()
+            {
+                PersonName = "Shoaib",
+                EmailAdd = "shoaibali.rts@gmail.com"
+            }));
+            WriteLine(obj.ToString());
+        }
+    }
+}
